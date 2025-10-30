974e7cbb35c435744044c8cbca596e0ba4729252
Daniel Vaquiro <danpipemon@gmail.com>
2025-10-29 19:45:30 -0500

    Se añaden los schemas; las estructuras de los datos

--- schemas.py
+++ schemas.py
@@ -15,16 +15,27 @@ class Curso(CursoBase):
     codigo: int
     estudiantes: List["Estudiante"] = [] #Relacion N:M
 
+#Crea curso
 class CrearCurso(CursoBase):
     pass
 
+#Actualiza la informacion de los cursos
+class ActualizarCurso(CursoBase):
+    codigo: Optional[int] = None
+    nombre: Optional[str] = None
+    creditos: Optional[int] = None
+    horario: Optional[str] = None
+
 #ESTUDIANTES
+
+#Atributos base de los estudiantes
 class EstudianteBase(BaseModel):
     cedula: int
     nombre: str
     email: str
     semestre: int
 
+#crea un estudiantes
 class CrearEstudiante(EstudianteBase):
     pass
 
@@ -33,9 +44,12 @@ class InfoEstudiante(EstudianteBase):
     id: int
     cursos: List[Curso] = [] #Realacion N:M
 
+#Actualiza la informacion de los estudiantes
 class ActualizarEstudiante(EstudianteBase):
     cedula: Optional[int] = None
     nombre: Optional[str] = None
     email: Optional[str] = None
     semestre: Optional[str] = None
+
+
 
